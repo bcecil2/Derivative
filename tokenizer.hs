f39558c1bd0305f3b0639a3ee16d853321bcbd97
Bcecil <blakemadness@gmail.com>
2019-12-15 21:43:28 -0800

    Upgraded toknum to float and implemented float parsing

--- tokenizer.hs
+++ tokenizer.hs
@@ -1,7 +1,9 @@
 import Data.Char
+import Data.List
+
 -- Primitive data types that will be used to build up expressions --
 data Operator = Plus | Minus | Times | Div deriving(Show, Eq)
-data Token = Tokop Operator | Tokid String | Toknum Int deriving(Show, Eq) 
+data Token = Tokop Operator | Tokid String | Toknum Float deriving(Show, Eq) 
 
 -- Helper function to print operators --
 showOp :: Operator -> String
@@ -24,6 +26,18 @@ createId :: String -> (String, String)
 createId expr = (identifier, rest)
 		where identifier = takeWhile (isAlpha) expr
 		      rest = drop (length identifier) expr
+
+-- Handles numbers that are multiple digits/decimals
+-- returns a tuple with the first element being the 
+-- number and the second element containing the rest
+-- of the input expression (can be empty)
+createNum :: String -> (String, String)
+createNum expr
+             | elem (length $ findIndices (=='.') expr) [0,1] = (num, rest)
+             | otherwise = error $ "could not tokenize numeric constant too many decimals " ++ num
+         where num  = takeWhile (\ x -> isDigit x || x == '.') expr
+               rest = drop (length num) expr
+
 -- Takes in a String and returns a list of tokens
 -- TODO: generalize Toknum to be a float and write a
 -- function similar to createId that constructs floats 
@@ -32,14 +46,15 @@ tokenize :: String -> [Token]
 tokenize [] = []
 tokenize (x:xs)
     | elem x "+-*/" = Tokop Plus : tokenize xs
-    | isAlpha x = Tokid (fst res) : tokenize (snd res) 
-    | isDigit x = Toknum (digitToInt x) : tokenize xs
+    | isAlpha x = Tokid (fst identifier) : tokenize (snd identifier) 
+    | isDigit x = Toknum (read $ fst number :: Float) : tokenize (snd number)
     | isSpace x = tokenize xs
     | otherwise = error $  "Could not tokenize " ++ [x]
-       where res =  createId(x:xs)
+       where identifier =  createId(x:xs)
+             number = createNum(x:xs)
 
 --test code w o r k s g r e a t
-expr = "R2D2+C3PO" 
+expr = "R22.323D420+C3PO" 
 
 
 main = do
